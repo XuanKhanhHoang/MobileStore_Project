355272e1740a031bbac05f65d9245054359ac333
Xuan Khanh Hoang <hoangxuankhanh.work@gmail.com>
2023-10-24 21:09:30 +0700

    generated model by scaffordDb

---
+++ MobileStore_Project/MobileStore_Project/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileStore_Project.Models
+{
+    public partial class Admin
+    {
+        public string Idadmin { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Passw { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
---
+++ MobileStore_Project/MobileStore_Project/Models/Nsx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileStore_Project.Models
+{
+    public partial class Nsx
+    {
+        public Nsx()
+        {
+            SanPhams = new HashSet<SanPham>();
+        }
+
+        public string MaNsx { get; set; } = null!;
+        public string? TenNsx { get; set; }
+
+        public virtual ICollection<SanPham> SanPhams { get; set; }
+    }
+}
