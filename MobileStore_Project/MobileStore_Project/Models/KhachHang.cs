f7aed18115f1bb6355f98945b78fed4e5256f57c
kain-1910 <pvcong1910@gmail.com>
2023-10-31 02:01:46 +0700

    data anotation in model KH

--- MobileStore_Project/MobileStore_Project/Models/KhachHang.cs
+++ MobileStore_Project/MobileStore_Project/Models/KhachHang.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MobileStore_Project.Models
 {
@@ -11,10 +12,16 @@ namespace MobileStore_Project.Models
         }
 
         public string Id { get; set; } = null!;
+        [Required(ErrorMessage = "Vui lòng nhập trường này")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@gmail\.com$", ErrorMessage = "Vui lòng nhập chính xác dữ liệu, email cần có gmail.com")]
         public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Vui lòng nhập trường này")]
         public string Passw { get; set; } = null!;
+        [Required(ErrorMessage = "Vui lòng nhập trường này")]
         public string HoTen { get; set; } = null!;
+        [Required(ErrorMessage = "Vui lòng nhập trường này")]
         public string? SoDt { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập trường này")]
         public string? DiaChi { get; set; }
 
         public virtual ICollection<DonHang> DonHangs { get; set; }
